2b21cf9011bb47056b3e1d63283ba4eb1e45129b
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-01 21:12:36 +0100

    shows in touch bar the number of clicks made trough it

--- TouchBarSpike/WindowController+NSTouchBarDelegate.swift
+++ TouchBarSpike/WindowController+NSTouchBarDelegate.swift
@@ -11,12 +11,23 @@ extension WindowController: NSTouchBarDelegate {
             custom.view = NSTextField.init(labelWithString: label)
             return custom
         case NSTouchBarItemIdentifier.button:
-            let label = "Click me!"
+            let label = buttonLabel()
             let custom = NSCustomTouchBarItem(identifier: identifier)
             custom.customizationLabel = label
-            custom.view = NSButton(title: label, target: self, action: #selector(clickMe))
+            custom.view = NSButton(title: label, target: self, action: #selector(touchBarButtonClicked))
             return custom
         default: return nil
+        }
+    }
+
+    func buttonLabel() -> String {
+        switch touchBarClicks {
+        case 0:
+            return "You still didn't clicked me 😭"
+        case 1:
+            return "You clicked me one time 😊"
+        default:
+            return "You clicked me \(touchBarClicks) times 😁"
         }
     }
 }
--- TouchBarSpike/WindowController.swift
+++ TouchBarSpike/WindowController.swift
@@ -2,6 +2,8 @@ import Cocoa
 
 class WindowController: NSWindowController {
 
+    var touchBarClicks = 0
+
     @available(OSX 10.12.1, *)
     override func makeTouchBar() -> NSTouchBar? {
         let touchBar = NSTouchBar()
@@ -12,7 +14,14 @@ class WindowController: NSWindowController {
         return touchBar
     }
 
-    func clickMe() {
-        print("Touch Bar was clicked!")
+    func touchBarButtonClicked() {
+        touchBarClicks += 1
+        invalidateTouchBar()
+    }
+
+    func invalidateTouchBar() {
+        if #available(OSX 10.12.1, *) {
+            touchBar = nil
+        }
     }
 }
